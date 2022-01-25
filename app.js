ead71923ed72c60780811799f0bc5af27e6dc20c
RenijS <renijshrestha8@gmail.com>
2022-01-25 13:49:58 +1100

    workings

--- app.js
+++ app.js
@@ -2,23 +2,45 @@ const sketchBoard = document.querySelector(".sketchBoard");
 const rangeValue = document.querySelector(".rangeValue");
 const areaSelector = document.getElementById("areaSelector");
 
-for (let i = 0; i < 256; i++) {
-  const myDiv = document.createElement("div");
-  const width = sketchBoard.clientWidth / 16;
-  const height = sketchBoard.clientHeight / 16;
-  console.log(
-    width +
-      " $$ " +
-      height +
-      " ,, " +
-      sketchBoard.clientWidth +
-      sketchBoard.clientHeight
-  );
-  myDiv.style.width = `${width}px`;
-  myDiv.style.height = `${height}px`;
-  sketchBoard.appendChild(myDiv);
+const colorSelector = document.getElementById("cSelect");
+const normalBtn = document.getElementById("normalMode");
+const randomBtn = document.getElementById("randomMode");
+const eraserBtn = document.getElementById("eraserMode");
+const clearBtn = document.getElementById("clearAll");
+
+function createBlock(blockAmount) {
+  for (let i = 0; i < blockAmount * blockAmount; i++) {
+    const myDiv = document.createElement("div");
+    myDiv.addEventListener(
+      "mouseover",
+      function (event) {
+        // highlight the mouseover target
+        event.target.style.backgroundColor = colorSelector.value;
+      },
+      false
+    );
+    myDiv.style.width = `${sketchBoard.clientWidth / blockAmount}px`;
+    myDiv.style.height = `${sketchBoard.clientHeight / blockAmount}px`;
+    sketchBoard.appendChild(myDiv);
+  }
 }
 
-areaSelector.addEventListener("input", (area) => {
-  rangeValue.textContent = areaSelector.value + " * " + areaSelector.value;
-});
+function clearBlock() {
+  sketchBoard.innerHTML = "";
+}
+
+createBlock(parseInt(areaSelector.value));
+
+areaSelector.onmousemove = (e) => {
+  rangeValue.textContent = e.target.value + " x " + e.target.value;
+};
+
+areaSelector.onchange = () => {
+  clearBlock();
+  createBlock(parseInt(areaSelector.value));
+};
+
+clearBtn.onclick = () => {
+  clearBlock();
+  createBlock(parseInt(areaSelector.value));
+};
